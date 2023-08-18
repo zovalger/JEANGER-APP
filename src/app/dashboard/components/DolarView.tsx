a96bdd228dbebdc6ff8cb01d116feccd5ba6263f
zovalger <zovalger@gmail.com>
2023-08-18 12:00:31 -0400

    fix 2: copy clipboard

--- src/app/dashboard/components/DolarView.tsx
+++ src/app/dashboard/components/DolarView.tsx
@@ -1,11 +1,13 @@
 "use client";
 
-import { useGlobalContext } from "@/contexts/Global.context";
 import { Box, IconButton, Typography } from "@mui/material";
 import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 import LibraryAddCheckIcon from "@mui/icons-material/LibraryAddCheck";
 import ReplayIcon from "@mui/icons-material/Replay";
 import { useState } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+
+import { useGlobalContext } from "@/contexts/Global.context";
 
 export default function DolarView() {
 	const { dolar, refreshDolar } = useGlobalContext();
@@ -35,9 +37,21 @@ export default function DolarView() {
 				<strong>Dolar: </strong>
 				{dolar && dolar.value.toFixed(2)}
 			</Typography>
-			<IconButton onClick={copyDolarValueToClipboard}>
-				{copy ? <LibraryAddCheckIcon /> : <ContentCopyIcon />}
-			</IconButton>
+
+			{dolar && (
+				<CopyToClipboard
+					text={dolar.value.toString().replace(".", ",")}
+					onCopy={() => {
+						setCopy(true);
+
+						setTimeout(() => setCopy(false), 1000);
+					}}
+				>
+					<IconButton>
+						{copy ? <LibraryAddCheckIcon /> : <ContentCopyIcon />}
+					</IconButton>
+				</CopyToClipboard>
+			)}
 			<IconButton onClick={refreshDolar}>
 				<ReplayIcon />
 			</IconButton>
