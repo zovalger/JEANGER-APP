2b78f6c7dc1de0d4e1c8ca66076e6393df7db3a2
Zovalger <74253343+Zovalger@users.noreply.github.com>
2024-03-29 11:26:27 -0400

    reafctory cosultMovilnet

--- src/app/dashboard/components/ConsultMovilnet.tsx
+++ src/app/dashboard/components/ConsultMovilnet.tsx
@@ -3,11 +3,14 @@
 import { useState } from "react";
 
 import TextField from "@mui/material/TextField";
-import { IconButton, Typography, Box } from "@mui/material";
+import { IconButton, Typography, Box, InputBase } from "@mui/material";
 import { getSaldoMovilnet_Request } from "@/api/Utility.api";
 import CopyToClipboard from "react-copy-to-clipboard";
 import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 import LibraryAddCheckIcon from "@mui/icons-material/LibraryAddCheck";
+import SearchIcon from "@mui/icons-material/Search";
+import CloseIcon from "@mui/icons-material/Close";
+import FormatSizeIcon from "@mui/icons-material/FormatSize";
 import { saldoMovilnet } from "@/types";
 
 export default function ConsultMovilnet() {
@@ -30,49 +33,93 @@ export default function ConsultMovilnet() {
 		}
 	};
 
+	const handdleClear = () => {
+		setValue("");
+		setSaldoMovilnet(null);
+	};
+
 	// const [copy, setCopy] = useState(false);
 
 	return (
-		<>
-			<Typography sx={{ my: 1.5 }} fontSize={"1.1rem"} variant="h6">
+		<Box
+			component={"form"}
+			onSubmit={(e) => {
+				e.preventDefault();
+
+				handdleSubmit();
+			}}
+			sx={{
+				boxShadow: 2,
+				p: 2,
+				borderRadius: "8px",
+				mb: 2,
+			}}
+		>
+			<Typography
+				sx={{
+					mb: 1,
+					fontWeight: 600,
+				}}
+			>
 				Consulta Movilnet
 			</Typography>
-			<Box sx={{ display: "flex" }}>
-				<TextField
-					label="Numero"
-					type="number"
-					placeholder="Numero"
-					variant="outlined"
+
+			<Box sx={{ display: "flex", alignItems: "center" }}>
+				Tlf.:
+				<InputBase
+					// label="Cédula"
+					type="tel"
+					placeholder="Número"
+					// variant="outlined"
 					autoComplete="none"
-					name="ci"
+					name="tlf"
 					onKeyDown={(event) => {
-						if (event.key === "Enter") handdleSubmit();
+						event.stopPropagation();
+						if (event.key === "Escape") handdleSubmit();
 					}}
 					value={value || ""}
 					onChange={({ target: { value } }) => {
 						handdleChange(value);
 					}}
+					sx={{
+						ml: 1,
+						flexGrow: 1,
+					}}
 				/>
+				{value && (
+					<IconButton size="small" onClick={handdleClear}>
+						<CloseIcon />
+					</IconButton>
+				)}
 				<CopyToClipboard
-					text={value.trim().replace(/^0/,"")}
+					text={value.trim().replace(/^0/, "")}
 					onCopy={() => {
 						setCopy(true);
 						setTimeout(() => setCopy(false), 1000);
 					}}
 				>
-					<IconButton sx={{ ml: 2 }}>
+					<IconButton size="small">
 						{copy ? <LibraryAddCheckIcon /> : <ContentCopyIcon />}
 					</IconButton>
 				</CopyToClipboard>
+				<IconButton size="small" type="submit">
+					<SearchIcon />
+				</IconButton>
 			</Box>
 
 			{saldoMovilnet && (
-				<Box sx={{ p: 1 }}>
-					<Typography>{saldoMovilnet.saldo}</Typography>
-					<Typography>{saldoMovilnet.status}</Typography>
-					<Typography>{saldoMovilnet.date}</Typography>
-				</Box>
+				<>
+					<Typography sx={{ fontWeight: 600 }}>
+						BS {saldoMovilnet.saldo}
+					</Typography>
+					<Typography component={"span"} fontSize={"small"} sx={{ mr: 1 }}>
+						{saldoMovilnet.status}
+					</Typography>
+					<Typography component={"span"} fontSize={"small"}>
+						{saldoMovilnet.date}
+					</Typography>
+				</>
 			)}
-		</>
+		</Box>
 	);
 }
